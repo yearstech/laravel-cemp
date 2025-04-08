bd4e802da687627f9b8d766cb3f03e2ba9aeb8f2
Yasin Uddowla <yasinuddowla@gmail.com>
2025-04-08 21:43:46 +0600

    rollback to basic laravel

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,13 @@
-import {
-    defineConfig
-} from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: [`resources/views/**/*`],
+            refresh: true,
         }),
         tailwindcss(),
     ],
-    server: {
-        cors: true,
-    },
 });
