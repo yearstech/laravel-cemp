a41a2bf5ae6a3d35edd4da4f353ec25f014ed8ac
Yasin Uddowla <yasinuddowla@gmail.com>
2025-04-12 22:43:56 +0600

    modifications

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,25 +1,27 @@
 @extends('layouts.app')
 @section('title', 'User Lists')
 @section('content')
-<h2>User Edit</h2>
-<form action="{{route('user.update',$user->id)}}" method="Post">
-    @csrf
-    <div class="form-group">
-        <label for="name">name</label>
-        <input type="name" class="form-control" id="name" name="name" value="{{ $user->name }}" required>
-    </div>
-    <div class="form-group">
-        <label for="name">Email</label>
-        <input type="email" class="form-control" id="email" name="email" value="{{ $user->email }}" required>
-    </div>
-    @foreach ($roles as $role)
-        <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="role-{{$role->id}}" {{($hasRoles->contains($role->id)) ? "checked" : ""}} value="{{$role->name}}" name="role[]">   
-            <label class="form-check-label" for="role-{{$role->id}}">{{$role->name}}</label>
+    <h2>User Edit</h2>
+    <form action="{{ route('user.update', $user->id) }}" method="post">
+        @csrf
+        @method('PATCH')
+        <div class="form-group">
+            <label for="name">name</label>
+            <input type="name" class="form-control" id="name" name="name" value="{{ $user->name }}" required>
         </div>
-    @endforeach    
+        <div class="form-group">
+            <label for="name">Email</label>
+            <input type="email" class="form-control" id="email" name="email" value="{{ $user->email }}" required>
+        </div>
+        @foreach ($roles as $role)
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="role-{{ $role->id }}"
+                    {{ $hasRoles->contains($role->id) ? 'checked' : '' }} value="{{ $role->name }}" name="role[]">
+                <label class="form-check-label" for="role-{{ $role->id }}">{{ $role->name }}</label>
+            </div>
+        @endforeach
 
-    <br>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+        <br>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Middleware\CheckIfLoggedIn;
+
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\SettingController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\RoleController;
-use App\Http\Middleware\CheckIfLoggedIn;
 use App\Http\Controllers\EventTypeController;
-use App\Models\EventType;
 
 Route::get('/event-types', [EventTypeController::class, 'index'])->name('event-types.index');
 Route::get('/event-types/create', [EventTypeController::class, 'create'])->name('event-types.create');  // Changed to match event-types.create
 Route::post('/event-types/store', [EventTypeController::class, 'store'])->name('event-types.store');  // Changed to match event-types.store
-
-
-
-
 
 // Auth
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
@@ -30,11 +26,23 @@ Route::get('/', [HomeController::class, 'index'])->name('dashboard')->middleware
 
 // User
 Route::middleware(CheckIfLoggedIn::class)->group(function () {
-    Route::get('/profile', [UserController::class, 'profile'])->name('user.profile');
-    Route::get('/userlist', [UserController::class, 'list'])->name('user.list');
-    Route::get('/useredit/{id}', [UserController::class, 'edit'])->name('user.edit');
-    Route::post('/userupdate/{id}', [UserController::class, 'update'])->name('user.update');
-    Route::post('/change-password', [UserController::class, 'changePassword'])->name('user.change-password');
+    Route::get('/users/profile', [UserController::class, 'profile'])->name('user.profile');
+    Route::get('/users', [UserController::class, 'list'])->name('user.list');
+    Route::get('/users/edit/{id}', [UserController::class, 'edit'])->name('user.edit');
+    Route::patch('/users/{id}', [UserController::class, 'update'])->name('user.update');
+    Route::post('users/change-password', [UserController::class, 'changePassword'])->name('user.change-password');
+});
+
+// Roles and Permissions
+Route::middleware(CheckIfLoggedIn::class)->group(function () {
+    Route::get('/permissons/index', [PermissionController::class, 'index'])->name('permissions.index');
+    Route::get('/permissons/create', [PermissionController::class, 'create'])->name('permissions.create');
+    Route::post('/permissons/store', [PermissionController::class, 'store'])->name('permissions.store');
+    Route::get('/permissons/lists', [PermissionController::class, 'index'])->name('permissions.lists');
+
+    Route::get('/roles/index', [RoleController::class, 'index'])->name('roles.index');
+    Route::get('/roles/create', [RoleController::class, 'create'])->name('roles.create');
+    Route::post('/roles/store', [RoleController::class, 'store'])->name('roles.store');
 });
 // Settings
 Route::middleware(CheckIfLoggedIn::class)->group(function () {
@@ -45,14 +53,4 @@ Route::middleware(CheckIfLoggedIn::class)->group(function () {
     Route::delete('/settings/{setting}', [SettingController::class, 'destroy'])->name('settings.destroy');
     Route::get('/settings/{setting}', [SettingController::class, 'edit'])->name('settings.edit');
     Route::patch('/settings/{setting}', [SettingController::class, 'update'])->name('settings.update');
-
-    Route::get('/permissons/index',[PermissionController::class,'index'])->name('permissions.index');
-    Route::get('/permissons/create',[PermissionController::class,'create'])->name('permissions.create');
-    Route::post('/permissons/store',[PermissionController::class,'store'])->name('permissions.store');
-    Route::get('/permissons/lists',[PermissionController::class,'index'])->name('permissions.lists');
-
-    Route::get('/roles/index',[RoleController::class,'index'])->name('roles.index');
-    Route::get('/roles/create',[RoleController::class,'create'])->name('roles.create');
-    Route::post('/roles/store',[RoleController::class,'store'])->name('roles.store');
-    // Route::get('/roles/lists',[RoleController::class,'index'])->name('roles.lists');
 });
