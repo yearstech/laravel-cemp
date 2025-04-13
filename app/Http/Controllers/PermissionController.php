17702a28980b34d07f5919993ba5ab17d59f7858
Yasin Uddowla <yasinuddowla@gmail.com>
2025-04-13 22:13:13 +0600

    remove lists view and add index

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -3,15 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-Use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Permission;
 // use App\Models\Permission;
 
 class PermissionController extends Controller
 {
     public function index()
     {
-        $permissions = Permission::orderBy('created_at','desc')->paginate(10); 
-        return view('permissions.lists',compact('permissions'));
+        $permissions = Permission::orderBy('created_at', 'desc')->paginate(10);
+        return view('permissions.index', compact('permissions'));
     }
     public function create()
     {
@@ -22,24 +22,15 @@ class PermissionController extends Controller
         $validated = $request->validate([
             'name' => 'required|unique:permissions|max:15',
         ]);
-        if($validated){
+        if ($validated) {
             Permission::create(['name' => $request->name]);
-            return redirect()->route('permissions.index')->with('success','Permission created successfully');
-
-        }else{
-            return redirect()->route('permissions.create')->with('error','Permission creation failed');
+            return redirect()->route('permissions.index')->with('success', 'Permission created successfully');
+        } else {
+            return redirect()->route('permissions.create')->with('error', 'Permission creation failed');
         }
     }
-    public function show($id)
-    {
-    }
-    public function edit($id)
-    {
-    }
-    public function update(Request $request, $id)
-    {
-    }
-    public function destroy($id)
-    {
-    }
+    public function show($id) {}
+    public function edit($id) {}
+    public function update(Request $request, $id) {}
+    public function destroy($id) {}
 }
