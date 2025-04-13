66196a8a2b921e9216d3a978b23ffc5bb41518b5
Yasin Uddowla <yasinuddowla@gmail.com>
2025-04-13 22:19:56 +0600

    permission datatable

--- resources/views/event-types/index.blade.php
+++ resources/views/event-types/index.blade.php
@@ -1,8 +1,9 @@
 <!-- resources/views/event-types/index.blade.php -->
 
-@extends('layouts.app')  <!-- Correct syntax for extending a layout -->
+@extends('layouts.app') <!-- Correct syntax for extending a layout -->
 
-@section('content')  <!-- Define the content section -->
+@section('content')
+    <!-- Define the content section -->
     <h1>Event Types List</h1>
     <a href="{{ route('event-types.create') }}" class="btn btn-primary">Create New Event Type</a>
 
@@ -22,21 +23,20 @@
                     <td>{{ $eventType->id }}</td>
                     <td>{{ $eventType->name }}</td>
                     <td>
-                        <a href="{{route('event_types.edit',$eventType->id)}}" class="btn btn-secondary">Edit</a>
-                        
+                        <a href="{{ route('event_types.edit', $eventType->id) }}" class="btn btn-secondary">Edit</a>
+
 
                     </td>
                     <td>
-                        <form action="{{route('event-type.destroy',$eventType->id)}}" method="POST" onsubmit="return confirm('Are you sure you want to delete this event type?');">
+                        <form action="{{ route('event-type.destroy', $eventType->id) }}" method="POST"
+                            onsubmit="return confirm('Are you sure you want to delete this event type?');">
                             @csrf
                             @method('DELETE')
-                            <button type="submit"  class="btn btn-danger">Delete</button>
+                            <button type="submit" class="btn btn-danger">Delete</button>
                         </form>
                     </td>
                 </tr>
             @endforeach
         </tbody>
     </table>
-
-
-@endsection  <!-- End the content section -->
+@endsection <!-- End the content section -->
