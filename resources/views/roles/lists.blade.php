7184a4d753089c4804102dd104cf4bc540e82494
Yasin Uddowla <yasinuddowla@gmail.com>
2025-04-22 23:02:00 +0600

    table

--- resources/views/roles/lists.blade.php
+++ resources/views/roles/lists.blade.php
@@ -2,38 +2,38 @@
 @section('title', 'Role Lists')
 @section('content')
     <h2>Roles Name</h2>
-        <form action="{{route('roles.store')}}" method="POST" >
-            @csrf
-            <div class="form-group mb-2">
-                <label for="name">Name</label>
-                <input type="text" class="form-control" id="name" name="name">
+    <form action="{{ route('roles.store') }}" method="POST">
+        @csrf
+        <div class="form-group mb-2">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" id="name" name="name">
+        </div>
+        @foreach ($data['permissions'] as $permission)
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="{{ $permission->name }}" name="permissions[]">
+                <label class="form-check-label" for="permissions">{{ $permission->name }}</label>
             </div>
-            @foreach ($data['permissions'] as $permission)
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="{{$permission->name}}" name="permissions[]">   
-                    <label class="form-check-label" for="permissions">{{$permission->name}}</label>
-                </div>
-            @endforeach 
-            <button type="submit" class="btn btn-primary mt-2">Submit</button>
-        </form>
+        @endforeach
+        <button type="submit" class="btn btn-primary mt-2">Submit</button>
+    </form>
 
     <table class="table datatable-basic">
         <thead>
-            <tr style="border: 1px solid black">
+            <tr>
                 <th>Id</th>
                 <th>Name</th>
                 <th>Permissions</th>
                 <th>Actions</th>
             </tr>
         </thead>
-        <tbody> 
-            <?php $count = 1;?>
+        <tbody>
+            <?php $count = 1; ?>
             @foreach ($data['roles'] as $role)
-                <tr style="border: 1px solid black">
-                    <td>{{$count}}</td>
-                    <td>{{ucfirst($role->name)}}</td>
+                <tr>
+                    <td>{{ $count }}</td>
+                    <td>{{ ucfirst($role->name) }}</td>
                     <td>
-                        {{@implode(', ', $role->permissions->pluck('name')->toArray())}}
+                        {{ @implode(', ', $role->permissions->pluck('name')->toArray()) }}
                         {{-- @foreach ($list->permissions as $permission)
                             <span class="badge badge-primary">{{ucfirst($permission->name)}}</span>
                         @endforeach --}}
--- resources/views/users/list.blade.php
+++ resources/views/users/list.blade.php
@@ -2,42 +2,42 @@
 @section('title', 'User Lists')
 @section('content')
     <h2>Add User</h2>
-        <form action="{{route('register')}}" method="POST" >
-            @csrf
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="form-group mb-2">
-                        <label for="name">Name</label>
-                        <input type="text" class="form-control" id="name" name="name">
-                    </div>
-                    <div class="form-group mb-2">
-                        <label for="name">Password <span class="text-danger">(Min:6 Character)</span></label>
-                        <input type="password" class="form-control" id="password" name="password">
-                    </div>
+    <form action="{{ route('register') }}" method="POST">
+        @csrf
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group mb-2">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" id="name" name="name">
                 </div>
-                <div class="col-md-6">
-                    <div class="form-group mb-2">
-                        <label for="name">Email</label>
-                        <input type="email" class="form-control" id="email" name="email">
-                    </div>
-                    <div class="form-group mb-2">
-                        <label for="name">Confirm Password</label>
-                        <input type="password" class="form-control" id="password_confirmation" name="password_confirmation">
-                    </div>
+                <div class="form-group mb-2">
+                    <label for="name">Password <span class="text-danger">(Min:6 Character)</span></label>
+                    <input type="password" class="form-control" id="password" name="password">
                 </div>
             </div>
-            
-            @foreach ($roles as $role)
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="{{$role->name}}" name="roles[]">   
-                    <label class="form-check-label" for="roles">{{$role->name}}</label>
+            <div class="col-md-6">
+                <div class="form-group mb-2">
+                    <label for="name">Email</label>
+                    <input type="email" class="form-control" id="email" name="email">
                 </div>
-            @endforeach 
-            <button type="submit" class="btn btn-primary mt-2">Submit</button>
-        </form>
+                <div class="form-group mb-2">
+                    <label for="name">Confirm Password</label>
+                    <input type="password" class="form-control" id="password_confirmation" name="password_confirmation">
+                </div>
+            </div>
+        </div>
+
+        @foreach ($roles as $role)
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="{{ $role->name }}" name="roles[]">
+                <label class="form-check-label" for="roles">{{ $role->name }}</label>
+            </div>
+        @endforeach
+        <button type="submit" class="btn btn-primary mt-2">Submit</button>
+    </form>
     <table class="table datatable-basic">
         <thead>
-            <tr style="border: 1px solid black">
+            <tr>
                 <th>Id</th>
                 <th>Name</th>
                 <th>Email</th>
@@ -46,13 +46,13 @@
             </tr>
         </thead>
         <tbody>
-            <?php $count = 1;?>
+            <?php $count = 1; ?>
             @foreach ($users as $user)
                 <tr>
-                    <td>{{$count}}</td>
-                    <td>{{ucfirst($user->name)}}</td>
-                    <td>{{ucfirst($user->email)}}</td>
-                    <td>{{$user->roles->pluck('name')->implode(', ')}}</td>
+                    <td>{{ $count }}</td>
+                    <td>{{ ucfirst($user->name) }}</td>
+                    <td>{{ ucfirst($user->email) }}</td>
+                    <td>{{ $user->roles->pluck('name')->implode(', ') }}</td>
                     <td>
                         <x-utils.edit-action :route="route('user.edit', $user->id)" />
                         <x-utils.delete-action :id="$user->id" />
@@ -60,6 +60,6 @@
                 </tr>
             @endforeach
         </tbody>
-    </table> 
+    </table>
     <x-utils.delete-modal title="Delete Role" url="{{ route('user.destroy', ':id') }}" />
 @endsection
