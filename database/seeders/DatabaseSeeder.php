95389ad002a4f0deb2ab9bb10c85c8716a32d1c2
Yasin Uddowla <yasinuddowla@gmail.com>
2025-04-22 22:34:04 +0600

    users seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,8 +22,18 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             SettingSeeder::class,
         ]);
+        // create regular users with same password
+        $users = User::factory()->count(3)->create([
+            'password' => bcrypt('12345'),
+        ]);
+        foreach ($users as $user) {
+            $user->assignRole('user');
+        }
+
+        // create event types
         $eventTypes = EventType::factory()->count(5)->create();
-        $users = User::factory()->count(3)->create();
+
+        // create events
         Event::factory()->count(10)->create([
             'event_type_id' => $eventTypes->random()->id,
             'host_user_id' => $users->random()->id,
